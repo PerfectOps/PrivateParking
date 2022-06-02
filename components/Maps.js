862f64b4e6d61679753a6da3c2c83e15330ce7ee
Artem Lugovoi <57660209+PerfectOps@users.noreply.github.com>
2022-06-02 21:50:52 +0300

    Update Maps.js

--- components/Maps.js
+++ components/Maps.js
@@ -8,7 +8,7 @@ import auth from '@react-native-firebase/auth';
 import ReactNativeForegroundService from '@supersami/rn-foreground-service';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-YaMap.init('b37481d4-ebd4-45f6-9379-f130139dd549'); // ключ API MapKit
+YaMap.init('API_KEY_MAPKIT'); // ключ API MapKit
 
 export default class Maps extends Component {
     constructor(props){
